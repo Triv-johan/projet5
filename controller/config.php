e4d8bb842d2f6ae8c40abc93bd9cf84961f4be9e
Triv-johan <guyon.johan@laposte.net>
2020-12-16 09:27:11 +0100

    maj

--- controller/config.php
+++ controller/config.php
@@ -1,9 +1,9 @@
 <?php
 // Informations d'identification
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'immeuble');
+define('DB_USERNAME', 'johang');
+define('DB_PASSWORD', '4E3r+JZ43Q7Kgw==');
+define('DB_NAME', 'johang_concierge');
  
 // Connexion à la base de données MySQL 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
